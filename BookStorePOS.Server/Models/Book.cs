76984c3b7865d863a7ce1e42fc48b5ff2e3ea731
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2024-12-27 11:30:03 -0400

    Add project files.

---
+++ BookStorePOS.Server/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStorePOS.Server.Models
+{
+    public class Book
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public required int PageCount { get; set; }
+
+        public int Excerpt { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+}
